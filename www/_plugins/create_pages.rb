922e1642eb56f619db2319fc7d9a08088cffa77f
Brock Fanning <brockfanning@gmail.com>
2020-02-01 18:00:48 -0500

    Change docs to www

---
+++ www/_plugins/create_pages.rb
@@ -0,0 +1,53 @@
+require "jekyll"
+
+module SdgMetadataPlugins
+  class CreatePages < Jekyll::Generator
+    safe true
+    priority :normal
+
+    def generate(site)
+      base = site.source
+
+      # Generate all the indicator pages.
+      site.data['all'].each do |language, indicators|
+        indicators.each do |indicator, indicator_fields|
+          dir = File.join('metadata', language, indicator) + '/'
+          layout = 'indicator'
+          title = indicator + ' - ' + language
+          content = indicator_fields['full']
+          data = {}
+          site.pages << SdgMetadataPage.new(site, base, dir, layout, title, content, language, data)
+        end
+      end
+
+      # Generate all the language pages.
+      site.data['all'].each do |language, indicators|
+        dir = File.join('metadata', language) + '/'
+        layout = 'language'
+        title = language
+        content = ''
+        language = language
+        data = {'indicators' => indicators.keys}
+        site.pages << SdgMetadataPage.new(site, base, dir, layout, title, content, language, data)
+      end
+    end
+  end
+
+  # A Page subclass used in the `CreatePages` class.
+  class SdgMetadataPage < Jekyll::Page
+    def initialize(site, base, dir, layout, title, content, language, data)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.data = {}
+      self.data['layout'] = layout
+      self.data['title'] = title
+      self.data['language'] = language
+      self.data.merge!(data)
+      self.content = content
+    end
+  end
+end
