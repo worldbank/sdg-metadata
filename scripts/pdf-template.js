fa1781e65b4186c644bcaa4bc342f8f05e6dca63
Brock Fanning <brockfanning@gmail.com>
2020-03-08 16:19:23 -0400

    Some PDF styling

--- scripts/pdf-template.js
+++ scripts/pdf-template.js
@@ -4,6 +4,22 @@ module.exports = (indicatorId, content) => `
     <head>
         <meta charset="utf-8" />
         <title>${ indicatorId }</title>
+        <style>
+            h1 {
+                border-bottom: 2px solid #BBB;
+                padding-bottom: 10px;
+              }
+            table {
+                border-collapse: collapse;
+            }
+            table, th, td {
+                border: 1px solid black;
+            }
+            th, td {
+                padding: 10px;
+                text-align: left;
+            }
+        </style>
     </head>
     <body>
         ${ content }
