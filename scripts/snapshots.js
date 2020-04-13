ed4c4b3d57ef59e1ac85661693ca8d56ea0e50d8
Brock Fanning <brockfanning@gmail.com>
2020-04-12 21:44:09 -0400

    Remove manual config changes

--- scripts/snapshots.js
+++ scripts/snapshots.js
@@ -22,21 +22,13 @@ git.branch((err, branchSummary) => {
             if (err) {
                 throw err
             }
-            // Make sure Jekyll builds using the right subfolder.
-            const subfolder = branch.replace(prefix, '')
-            const baseUrl = '/sdg-metadata/' + subfolder
-            const configPath = path.join(branch, 'www', '_config.yml')
-            let config = fs.readFileSync(configPath, 'utf8')
-            config = config.replace(': /sdg-metadata', ': ' + baseUrl)
-            // Also turn on the alert.
-            config = config + '\n\ntest_banner: true\n'
-            fs.writeFileSync(configPath, config, 'utf8')
 
             exec('cd ' + branch + ' && make build', (err, stdout, stderr) => {
                 if (err) {
                     console.error(err)
                 }
                 else {
+                    const subfolder = branch.replace(prefix, '')
                     const source = path.join(branch, 'www', '_site')
                     const destination = path.join('www', subfolder)
                     fs.rename(source, destination, function (err) {
