abba246b5818dc390ef557355260d134df87fc8d
Brock Fanning <brockfanning@gmail.com>
2020-04-14 17:29:19 -0400

    New way of converting PDFs, and start to clear out field order stuff

--- scripts/import-indicators.js
+++ scripts/import-indicators.js
@@ -1,9 +1,19 @@
-/**
- * This is a placeholder for a script to import from a metadata authoring tool.
- */
-
-const fs = require('fs')
+const {
+  WordTemplateInput,
+  GettextOutput
+} = require('sdg-metadata-convert')
 const path = require('path')
-const filePath = path.join('indicators', 'test.txt')
-const fileContents = 'testing'
-fs.writeFileSync(filePath, fileContents)
+const fs = require('fs')
+
+const sourceFolder = 'indicators'
+const targetFolder = path.join('translations', 'templates')
+const files = fs.readdirSync(sourceFolder).filter(file => {
+    return path.extname(file).toLowerCase() === '.docx';
+})
+
+for (const sourceFile of files) {
+    const sourcePath = path.join(sourceFolder, sourceFile)
+    const targetFile = sourceFile.replace('.docx', '.pot')
+    const targetPath = path.join(targetFolder, targetFile)
+    new WordTemplateInput(sourcePath).convertTo(new GettextOutput(targetPath))
+}
