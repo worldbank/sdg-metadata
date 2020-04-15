bc35641019dfbfc22fe9a95dcde2e30cdbe9eea0
Brock Fanning <brockfanning@gmail.com>
2020-04-15 12:55:03 -0400

    Update import-indicators to use new Promises API from sdg-metadata-convert.

--- scripts/import-indicators.js
+++ scripts/import-indicators.js
@@ -1,19 +1,33 @@
-const {
-  WordTemplateInput,
-  GettextOutput
-} = require('sdg-metadata-convert')
 const path = require('path')
 const fs = require('fs')
+
+const sdgMetadataConvert = require('sdg-metadata-convert')
+const wordTemplateInput = new sdgMetadataConvert.WordTemplateInput()
+const gettextOutput = new sdgMetadataConvert.GettextOutput()
 
 const sourceFolder = 'indicators'
 const targetFolder = path.join('translations', 'templates')
 const files = fs.readdirSync(sourceFolder).filter(file => {
     return path.extname(file).toLowerCase() === '.docx';
 })
-
-for (const sourceFile of files) {
+const conversions = files.map(sourceFile => {
     const sourcePath = path.join(sourceFolder, sourceFile)
     const targetFile = sourceFile.replace('.docx', '.pot')
     const targetPath = path.join(targetFolder, targetFile)
-    new WordTemplateInput(sourcePath).convertTo(new GettextOutput(targetPath))
+    return [sourcePath, targetPath]
+})
+
+importIndicators()
+
+async function importIndicators() {
+    for (const conversion of conversions) {
+        const [inputFile, outputFile] = conversion
+        try {
+            const metadata = await wordTemplateInput.read(inputFile)
+            await gettextOutput.write(metadata, outputFile)
+            console.log(`Converted ${inputFile} to ${outputFile}.`);
+        } catch(e) {
+            console.log(e)
+        }
+    }
 }
