5f0a454c41c90957689d07592385373ab7734752
Brock Fanning <brockfanning@gmail.com>
2020-11-13 08:54:33 -0500

    Temp

--- scripts/build/history.js
+++ scripts/build/history.js
@@ -2,17 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const git = require('simple-git')()
 
+const sdgMetadataConvert = require('sdg-metadata-convert')
+const wordTemplateInput = new sdgMetadataConvert.WordTemplateInput({debug:true})
+
 main()
 
 async function main() {
     for (const file of fs.readdirSync('indicators')) {
         const filePath = path.join('indicators', file)
+        console.log(filePath)
         git.log({ file: filePath }, (err, log) => {
             const commits = log.all.map(l => l.hash)
-            if (commits.length > 1) {
-                console.log(commits)
-                console.log(filePath)
-            }
+            const metadataObjects = commits.map(commit => convertCommitToMetadata(commit, filePath))
+            //console.log(metadataObjects)
         })
+        break
     }
+}
+
+async function convertCommitToMetadata(commit, filePath) {
+    await git.checkout(commit, [filePath])
 }
